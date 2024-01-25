bc38cec20649afc41e2aa02b27a6a5c75a594225
MasihTak <masihtak@outlook.com>
2024-01-25 14:35:40 +0100

    fix: displaying error when run out of the questions

--- lib/quiz.dart
+++ lib/quiz.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+
+import 'package:quiz_app/data/questions.dart';
 import 'package:quiz_app/start_screen.dart';
 import 'package:quiz_app/questions_screen.dart';
 
@@ -13,7 +15,7 @@ class Quiz extends StatefulWidget {
 
 class _QuizState extends State<Quiz> {
   var activeScreen = 'start-screen';
-  final List<String> selectedAnswers = [];
+  List<String> selectedAnswers = [];
 
   void switchScreen() {
     setState(() {
@@ -22,9 +24,14 @@ class _QuizState extends State<Quiz> {
   }
 
   void chooseAnswer(String answer) {
-    setState(() {
-      selectedAnswers.add(answer);
-    });
+    selectedAnswers.add(answer);
+
+    if (selectedAnswers.length == questions.length) {
+      setState(() {
+        selectedAnswers = [];
+        activeScreen = 'start-screen';
+      });
+    }
   }
 
   @override
